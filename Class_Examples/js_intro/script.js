afa5f2fe90066c95b82381e540082506f6f4dffe
Miriam Ruixz <Miriamruiz99@icloud.com>
2019-10-30 13:21:09 -0400

    css redo
    correcting my css assignment

---
+++ Class_Examples/js_intro/script.js
@@ -0,0 +1,16 @@
+var array1= [1,2,,3,4,5,6];
+var array2=["This is a string".'This is also a string']
+var heading = document.getEelementByTag("h1");
+console.log(heading);
+console.log(x);
+console.log(document.getEelementByTagName("h1"));
+$(documnent).ready(function () {
+  var x=2+2;
+  // be carefule where you put your quatatoins:
+  var y = 2+ "2";
+  //defining a variable
+  var z;
+  z="My name is Miriam"
+console.log("Everything loaded!");
+console.log("this function ran");
+});
